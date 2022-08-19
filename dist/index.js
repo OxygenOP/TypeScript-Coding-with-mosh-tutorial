751dd267fa4daf0080778d6a9dfc3395d94277ea
Hassam <hassamsohail709@gmail.com>
2022-08-19 21:02:37 +0300

    final

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,36 @@
 "use strict";
-let age = 18;
-if (age < 50) {
-    age += 10;
-    console.log("end");
+let sales = 123456;
+let course = "Typescript";
+let isPublished = true;
+let numbersArray = [1, 2, 13551];
+let user = [0, "Hassam Sohail"];
+let mySize = 2;
+console.log("mySize", mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
 }
+console.log(calculateTax(51000, 2023));
+let employee = {
+    id: 5,
+    name: "Hassam",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+let arry = [];
+arry.push({ id: 0, name: "Hassam" });
+let customArray = [];
+customArray.push({ id: 5, name: "Hassam" });
+customArray.push({ id: 7, name: "Hassam s" });
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+let quanitiy = 100;
+let size = "cm";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,78 @@
-// Type
+let sales: number = 123_456;
+let course: string = "Typescript";
+let isPublished: boolean = true;
 
-let age: number = 18;
+//Arrays
+let numbersArray: number[] = [1, 2, 135_51];
 
-if (age < 50) {
-  age += 10;
-  console.log("end");
+//Tuples
+let user: [number, string] = [0, "Hassam Sohail"];
+
+//Tuples are fixed Length Arrays
+//It is recommened to use it two values and not more (Objects are better for those)
+//let user:[number, string] = [0, "Hassam Sohail", 13] == Error
+
+//Enums
+const enum ShirtSize {
+  Small = 0,
+  Medium,
+  Large,
 }
+
+let mySize: ShirtSize = ShirtSize.Large;
+console.log("mySize", mySize);
+//
+
+//Function
+function calculateTax(income: number, taxYear: number = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+console.log(calculateTax(51_000, 2023));
+
+//Object
+let employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void; // Function in typescript Object
+} = {
+  id: 5,
+  name: "Hassam",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+//Array of Objects
+let arry: { id: number; name: string }[] = [];
+arry.push({ id: 0, name: "Hassam" });
+
+//Defining a Type
+type Employee = {
+  readonly id: number;
+  name: string;
+};
+
+//Array of Objects using Custom Type
+let customArray: Employee[] = [];
+customArray.push({ id: 5, name: "Hassam" });
+customArray.push({ id: 7, name: "Hassam s" });
+
+//Union Type
+function kgToLbs(weight: number | string): number {
+  //Narrowing
+  //Here I am narrowing the type to number to get all the methods available for a number type
+  // and the else by default would be string providing all the methods for string type
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+
+//Literal
+let quanitiy: 50 | 100 = 100;
+
+type Metric = "cm" | "inch";
+
+let size: Metric = "cm";
